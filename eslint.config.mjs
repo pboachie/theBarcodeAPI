16621209e56446039b5c1827c7c7121de4ecfd48
pboachie <princeboachie@gmail.com>
2024-10-18 07:43:22 -0700

    Refactor eslint configuration to support JSX and update parser options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,25 @@
 import js from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
 import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
 import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
 
 export default [
   {
     files: ['**/*.{js,mjs,cjs,jsx,tsx}'],
     languageOptions: {
+      parser: babelParser,
       ecmaVersion: 2021,
       sourceType: 'module',
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
       },
