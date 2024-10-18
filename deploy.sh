4eb2cf416653e2918a8f0758c4d1af2f03cdf5e4
pboachie <princeboachie@gmail.com>
2024-10-18 07:55:23 -0700

    Refactor deploy.sh to improve code readability and define absolute paths

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-
 set -e # Exit on error
 
+# Define absolute paths
 DEPLOY_DIR="/opt/thebarcodeapi"
 RELEASE_DIR="${DEPLOY_DIR}/releases"
 CURRENT_LINK="${DEPLOY_DIR}/current"
-NEW_RELEASE="${RELEASE_DIR}/release-$(date +%Y%m%d%H%M%S)"
-BUILD_DIR="./build"
+BUILD_DIR="$(pwd)/build"
 
 # Create new release directory
+sudo mkdir -p "${RELEASE_DIR}"
+NEW_RELEASE="${RELEASE_DIR}/release-$(date +%Y%m%d%H%M%S)"
 sudo mkdir -p "${NEW_RELEASE}"
 
-# Create the release and current directories if they don't exist
-sudo mkdir -p "${RELEASE_DIR}"
-sudo mkdir -p "${CURRENT_LINK}"
-
 # Copy new files to release directory
-sudo cp -R ${BUILD_DIR}/* "${NEW_RELEASE}"
+sudo cp -R "${BUILD_DIR}/." "${NEW_RELEASE}/"
 
 # Switch to new release
 sudo ln -sfn "${NEW_RELEASE}" "${CURRENT_LINK}"
