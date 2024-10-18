c65e60992dc8c4148a3ecebb160f3ee234e9b428
pboachie <princeboachie@gmail.com>
2024-10-17 22:25:08 -0700

    Refactor store.js to add barcodeReducer to the Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 // src/app/store.js
+
 import { configureStore } from '@reduxjs/toolkit';
+import barcodeReducer from '../slices/barcodeSlice';
 
 export const store = configureStore({
   reducer: {
-    // Todo: add reducers here
+    barcode: barcodeReducer,
   },
 });
