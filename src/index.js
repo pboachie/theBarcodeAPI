fa49a8dc3d02453c18cda5e3fd42de29e560cf6c
pboachie <princeboachie@gmail.com>
2024-10-17 21:02:26 -0700

    Add Tailwind CSS configuration and import styles in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
