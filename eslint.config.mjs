e5716e26d3604cd480a5bdc39afcfcb1c710fa2d
pboachie <princeboachie@gmail.com>
2024-10-18 00:24:09 -0700

    Refactor eslint configuration and add new eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,34 @@
 // eslint.config.mjs
 
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ];
