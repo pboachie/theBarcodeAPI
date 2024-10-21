1c5b0cbfe1fff58e83a8735da8139c3403b4d208
pboachie <princeboachie@gmail.com>
2024-10-20 20:04:39 -0700

    Refactor application-cd.yml to optimize PM2 process management, remove version from PM2 process name, optimize production dependencies installation, and clean up build artifacts

---
+++ pm2-version.js
@@ -0,0 +1,17 @@
+const pmx = require('@pm2/io');
+
+pmx.initModule({
+  widget: {
+    type: 'generic',
+    logo: 'https://app.pm2.io/icons/cpu.png',
+    theme: ['#111111', '#1B2228', '#31C2F1'],
+    el: {
+      probes: [
+        {
+          name: 'Version',
+          value: () => process.env.APP_VERSION || 'N/A'
+        }
+      ]
+    }
+  }
+});
