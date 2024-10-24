0c957bd8fec5440da0469b76c1b204ee0866b539
pboachie <princeboachie@gmail.com>
2024-10-23 18:09:52 -0700

    Add config.py file for managing application settings and rate limits

---
+++ barcodeAPI/app/config.py
@@ -0,0 +1,34 @@
+# app/config.py
+import os
+from app.schemas import BatchPriority
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    MASTER_API_KEY: str = os.getenv("MASTER_API_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    API_VERSION: str = os.getenv("API_VERSION")
+
+
+    class Config:
+        env_file = ".env"
+
+    class RateLimit:
+        unauthenticated: int = 10000
+
+        class Tier:
+            unauthenticated: int = 10000
+            basic: int = 50000
+            standard: int = 150000
+            premium: int = 500000
+
+settings = Settings()
+
+class OperationConfig:
+    def __init__(self, priority: BatchPriority, max_batch_size: int):
+        self.priority = priority
+        self.max_batch_size = max_batch_size
+
