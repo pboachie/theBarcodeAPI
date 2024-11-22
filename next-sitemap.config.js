dd42546fed743d29fa99172d00dec5cf51a57789
pboachie <princeboachie@gmail.com>
2024-11-21 21:00:06 -0800

    Update sitemap configuration with daily change frequency, priority, and additional sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,14 @@
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.thebarcodeapi.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.thebarcodeapi.com/sitemap.xml',
+      'https://www.thebarcodeapi.com/sitemap-0.xml',
+    ],
+  },
 };
